16775681c226afcd82221ca833a83054c4cb4dee
Brandon Corbett <Bccorb@users.noreply.github.com>
2026-04-04 09:26:18 -0400

    fix: remove auth from default

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF > /usr/share/nginx/html/config.js
 window.__SEAMLESS_CONFIG__ = {
-  API_URL: "${API_URL:-http://localhost:3000/auth}",
+  API_URL: "${API_URL:-http://localhost:3000/}",
   AUTH_MODE: "${AUTH_MODE:-server}"
 };
 EOF
